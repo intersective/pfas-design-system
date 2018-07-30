2689974c9523fa26dc82349fbf11e22ea2dc1782
Aen <hello@aentan.com>
2018-07-31 02:15:02 +0800

    refactor gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,16 @@ var gulp         = require("gulp"),
     uglify       = require('gulp-uglify'),
     cssmin       = require('gulp-cssmin'),
     changed      = require('gulp-changed'),
-    replace      = require('gulp-replace')
-    // gzip         = require('gulp-gzip')
+    replace      = require('gulp-replace'),
+    cache        = require('gulp-cache')
 
-// Compile SCSS files to CSS
+    // Compile SCSS files to CSS
 gulp.task("scss", function () {
 
   //Delete our old css files
   del(["static/css/**/*"])
 
-  // Clean SVG data
-  gulp.src("static-src/scss/abstracts/_svg-data.scss", { base: "."})
-    .pipe(replace('#', '%23'))
-    .pipe(gulp.dest("./"))
-
-  //compile hashed css files
+  // Combine SCSS files
   gulp.src(["static-src/vendor/css/**/*", "static-src/scss/main.scss"])
     .pipe(sass({
       outputStyle : "expanded"
@@ -33,42 +28,47 @@ gulp.task("scss", function () {
     }))
     .pipe(concat('main.css'))
     // .pipe(cssmin())
-    // .pipe(gzip())
     .pipe(gulp.dest("static/css"))
 })
 
 gulp.task("img", function () {
-  del(["static/img/**/*"])
+  // del(["static/img/**/*"])
   gulp.src("static-src/img/**/*")
-    .pipe(changed("static/img"))
-    .pipe(imagemin())
-    // .pipe(gzip())
+    .pipe(cache(imagemin()))
     .pipe(gulp.dest("static/img"))
 })
 
-// Hash SVG
+// SVG
 gulp.task("svg", function () {
   del(["static/svg/**/*"])
   gulp.src("static-src/svg/**/*")
-    .pipe(svgmin())
+    .pipe(cache(svgmin()))
     .pipe(gulp.dest("layouts/partials/svg"))
 })
 
-// Hash javascript
+// Compile SCSS files to CSS
+gulp.task("svg-data", function () {
+  // Clean SVG data
+  gulp.src("static-src/scss/abstracts/_svg-data.scss", { base: "." })
+    .pipe(replace('#', '%23'))
+    .pipe(gulp.dest("./"))
+})
+
+// Javascript
 gulp.task("js", function () {
   del(["static/js/**/*"])
   gulp.src(["static-src/vendor/js/**/*", "static-src/js/**/*"])
     .pipe(concat('main.js'))
     // .pipe(uglify())
-    // .pipe(gzip())
     .pipe(gulp.dest("static/js"))
 })
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss", "img", "svg", "js"], function () {
+gulp.task("watch", ["scss", "img", "svg", "svg-data", "js"], function () {
   gulp.watch(["static-src/vendor/css/**/*", "static-src/scss/**/*"], ["scss"])
   gulp.watch("static-src/img/**/*", ["img"])
-  gulp.watch("static-src/svg/**/*", ["svg"])
+  gulp.watch(["static-src/svg/**/*", "!static-src/scss/abstracts/_svg-data.scss"], ["svg"])
+  gulp.watch("static-src/scss/abstracts/_svg-data.scss", ["svg-data"])
   gulp.watch(["static-src/vendor/js/**/*", "static-src/js/**/*"], ["js"])
 })
 
