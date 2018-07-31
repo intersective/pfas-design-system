033aec44dfbaf50a7671df02215328bea55727fe
Aen <hello@aentan.com>
2018-07-31 11:24:06 +0800

    prepare for gh pages

--- _scripts/deploy-gh.sh
+++ _scripts/deploy-gh.sh
@@ -27,7 +27,7 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public
-echo yandp.aenism.com >> CNAME
+echo pfasds.aenism.com >> CNAME
 git add --all
 git commit -m "Publishing to gh-pages (deploy.sh)"
 
